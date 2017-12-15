784e3dd9d0e8c1ffcfd3dfbfc9474becfe2342b7
soyeyoman <steve.oyeyo@gmail.com>
2017-12-15 17:31:11 +0000

    header routing

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span> 
                   </button>
-                <a class="navbar-brand" style="float: left !important;" href="#" >Socio</a>
+                <a class="navbar-brand" style="float: left !important;" href="index.php" >Socio</a>
                         <div class="search-div" style="max-width: 40% !important;float: left;margin-top: 10px ">
                             <input type="text" id="search" class="search-input form-control" style="width:140% !important;"> 
                             <ul class="list-group suggest-box" style="position: absolute;width: 40% !important;">
@@ -33,18 +33,15 @@
              <div class="head-p2 col-md-6">
                  <div class="collapse navbar-collapse" id="collap">
                     <ul class="nav navbar-nav">
-               <li><a>TIMELINE</a></li>
-               <li><a>MESSAGES</a></li>
-               <li><a>NOTIFICATIONS</a></li>
+               <li><a href="index.php">TIMELINE</a></li>
+               <li><a href="messages.php">MESSAGES</a></li>
+               <li><a href="notify.php">NOTIFICATIONS</a></li>
                <li><div class="dropdown">
                      <button class="btn dropdown-toggle" type="button" data-toggle="dropdown">User<span class="caret"></span></button>  
                    <ul class="dropdown-menu">
-                   <li><a>My Profile</a></li>
-                   <li><a>Timeline</a></li>
-                    <li><a>Messages</a></li>
-                    <li><a>Notifications</a></li>
-                    <li><a>My Account</a></li>
-                    <li><a>Logout</a></li>
+                   <li><a href="profile.php" >My Profile</a></li>
+                    <li><a href="my-account.php" >My Account</a></li>
+                    <li><a href="logout.php">Logout</a></li>
                    </ul>
                    </div></li>
               </ul>
--- my-account.php
+++ my-account.php
@@ -1,24 +1,23 @@
-
 <?php
-
- function __autoload($class_name){
+   function __autoload($class_name){
        if (file_exists("./classes/".$class_name.".php")) {
         require_once("./classes/".$class_name.".php");
-  }
-}
-if(!Login::isloggedin()){
+     }
+   }  
+
+   //redirect if not logged in
+   if(!Login::isloggedin()){
      header("Location:login.php");
    }
-   
-  if(isset($_POST['profimg'])){
-    $username = Login::getUserName();
-    $userid = Login::getUserId();
-  	
-    Image::ImageUpload($username,"profileimg","profile",$userid);
-  }
-?>
-<h1>My Account</h1>
-<form method="post" action="my-account.php" enctype="multipart/form-data">
-   <input type="file" name="profileimg">
-   <input type="submit" name="profimg">
-</form>
+   $title = "Timeline";
+
+   include 'includes/header.php';
+?> 
+<div id="full-page">
+  
+
+</div>
+  
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
