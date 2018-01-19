8cd94000caf21aadb90e0bf14635d3919668b9fd
soyeyoman <steve.oyeyo@gmail.com>
2018-01-19 16:29:12 +0000

    minor touches

--- my-account.php
+++ my-account.php
@@ -14,7 +14,31 @@
    include 'includes/header.php';
 ?> 
 <div id="full-page">
-    
+     <div class="container">
+            <div class="container text-center" style="margin-top: 5em;width: 50%">
+                <form>
+                    <div class="form-group">
+                        <label for="username">UserName</label>
+                        <input name="username" type="text" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="about">About Me</label>
+                        <textarea class="form-control" name="about" rows="4" resize="fasle"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="profilepic">Profile Pic</label><div class="clearfix"></div>
+                        <img class="img-circle img-responsive" src="prof.jpg">
+                        <input type="file" name="profilepic" class="form-control">
+                    </div>
+                      <div class="form-group">
+                          <label for="thumbpic">Profile Pic </label>
+                          <img class="img-thumb img-rounded img-responsive" src="prof.jpg" >
+                        <input type="file" name="thumbpic" class="form-control">
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
 </div>
   
 <?php include 'includes/footer.php'; ?>
